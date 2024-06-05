b9b773f6cb1185e9d1b1a94d0eac6531eeef42b9
Bharatp6 <143147688+Bharatp6@users.noreply.github.com>
2024-06-05 19:27:01 -0400

    Update main.tf

--- tf_create_cf/main.tf
+++ tf_create_cf/main.tf
@@ -95,13 +95,4 @@ output "url" {
   value = google_cloud_run_service.default.status[0].url
 }
 
-variable "project_id" {
-  description = "The project ID to deploy the resources in"
-  type        = string
-}
 
-variable "region" {
-  description = "The region to deploy the resources in"
-  type        = string
-  default     = "us-central1"
-}
