80ca78d1c67a4b2f88088a147e3a8b6f9ca15276
Bharatp6 <143147688+Bharatp6@users.noreply.github.com>
2024-06-11 18:11:12 -0400

    Create variables.tf

---
+++ tf_create_pubsub/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The ID of the GCP project to use."
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region to use."
+  type        = string
+}
