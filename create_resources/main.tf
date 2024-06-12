1124414a4670de17254c23ac2d426d820cbf8f9a
Bharatp6 <143147688+Bharatp6@users.noreply.github.com>
2024-06-12 15:38:30 -0400

    Update main.tf

--- create_resources/main.tf
+++ create_resources/main.tf
@@ -39,7 +39,7 @@ resource "google_cloudfunctions2_function" "function" {
   service_config {
     service_account_email = var.service_account_email # "cloud-function-service-account@${var.project_id}.iam.gserviceaccount.com"
     min_instance_count    = 0
-    max_instance_count    = 1
+    max_instance_count    = 2
     available_memory      = "128Mi"
     ingress_settings      = "ALLOW_ALL"
   }
@@ -54,4 +54,42 @@ output "url" {
 resource "google_pubsub_topic" "station_status" {
   name = "station-status"
   message_retention_duration = "86600s"
+}
+
+# Create a Cloud Storage bucket for function code
+resource "google_storage_bucket" "function_bucket_pull" {
+  project       = var.project_id
+  name          = "${var.project_id}-function-bucket-pull"
+  location      = var.region
+  force_destroy = false  # Set to false to prevent bucket deletion
+}
+
+# Upload the function code to the bucket
+resource "google_storage_bucket_object" "function_zip1" {
+  name   = "pull_station_stat.zip"
+  bucket = google_storage_bucket.function_bucket_pull.name
+  source = "${path.module}/pull_station_stat.zip"
+}
+
+# Create a Cloud Function for pulling the station status
+resource "google_cloudfunctions_function" "function" {
+  name        = "pull_station_status"
+  runtime     = "python310"  # Adjust the runtime according to your code
+  entry_point = "pull_station_status"
+  source_archive_bucket = google_storage_bucket.function_bucket_pull.name
+  source_archive_object = google_storage_bucket_object.function_zip1.name
+  trigger_http = false
+
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = google_pubsub_topic.station_status.id
+  }
+
+  available_memory_mb = 256  # Set the memory in MB (e.g., 128, 256, 512, 1024, 2048, etc.)
+  min_instances       = 0    # Set the minimum number of instances
+  max_instances       = 2    # Set the maximum number of instances
+
+  environment_variables = {
+    PUBSUB_TOPIC = google_pubsub_topic.station_status.name
+  }
 }
