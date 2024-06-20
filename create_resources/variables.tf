30fa7c364a55e1d460fa78f8fb1fe2604b353513
Bharatp6 <143147688+Bharatp6@users.noreply.github.com>
2024-06-20 19:49:25 -0400

    Update variables.tf

--- create_resources/variables.tf
+++ create_resources/variables.tf
@@ -19,10 +19,10 @@ variable "dataset_id" {
   default     = "my_dataset"
 }
 
-variable "google_cloud_keyfile_json" {
-    description = "The cloud credentials"
-    type = string
-}
+#variable "google_cloud_keyfile_json" {
+#    description = "The cloud credentials"
+#    type = string
+#}
 
 variable "station_status_nrt_schema" {
   description = "Schema for STATION_STATUS_NRT table"
