7d7e9d129ca189614abdd57b269645792545574b
Bharatp6 <143147688+Bharatp6@users.noreply.github.com>
2024-06-20 16:40:22 -0400

    Update main.tf

--- create_resources/main.tf
+++ create_resources/main.tf
@@ -12,7 +12,6 @@ resource "google_storage_bucket" "tf_files" {
 terraform {
   backend "gcs" {
     bucket  = "terraform_files_"
-    key= "key/terraform.tfstate"
     prefix  = "terraform_state"
   }
 }
