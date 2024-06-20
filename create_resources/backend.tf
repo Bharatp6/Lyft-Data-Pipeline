74747b6afead1f58566874cc582066af875dbe63
Bharatp6 <143147688+Bharatp6@users.noreply.github.com>
2024-06-20 19:49:49 -0400

    Update backend.tf

--- create_resources/backend.tf
+++ create_resources/backend.tf
@@ -1,6 +1,6 @@
-terraform {
-  backend "gcs" {
-    bucket      = "terraform_state_st"
-    credentials = var.google_cloud_keyfile_json
-  }
-}
+#terraform {
+#  backend "gcs" {
+#    bucket      = "terraform_state_st"
+#    credentials = var.google_cloud_keyfile_json
+#  }
+#}
