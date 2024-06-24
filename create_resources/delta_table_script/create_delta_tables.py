20ebb2964a76c2240bce9b8e4af6541d4150972c
Bharatp6 <143147688+Bharatp6@users.noreply.github.com>
2024-06-23 20:07:36 -0400

    Update create_delta_tables.py

--- create_resources/delta_table_script/create_delta_tables.py
+++ create_resources/delta_table_script/create_delta_tables.py
@@ -9,7 +9,7 @@ from delta import *
 builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-    .config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.5") 
+    #.config("spark.jars.packages", "com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.5") 
     # .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
     # .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", "/path/to/gcloud-service-key.json")
 
