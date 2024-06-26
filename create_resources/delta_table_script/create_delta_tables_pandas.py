4a041effd34f964dd24867b21fb360d9fb21acbd
Bharatp6 <143147688+Bharatp6@users.noreply.github.com>
2024-06-26 18:49:58 -0400

    Update create_delta_tables_pandas.py

--- create_resources/delta_table_script/create_delta_tables_pandas.py
+++ create_resources/delta_table_script/create_delta_tables_pandas.py
@@ -9,55 +9,55 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcloud-service-key.json"
 
 # Define the schema and create empty DataFrames
 schema_station_status_nrt = {
-    "datetime": "datetime64[ns]",
-    "station_id": "object",
-    "num_bikes_available": "int64",
-    "vehicle_type_id_1": "int64",
-    "vehicle_type_id_2": "int64",
-    "num_ebikes_available": "int64",
-    "num_docks_available": "int64",
-    "num_docks_disabled": "int64",
-    "num_bikes_disabled": "int64",
-    "num_scooters_available": "int64",
-    "num_scooters_unavailable": "int64",
-    "is_installed": "boolean",
-    "is_renting": "boolean",
-    "is_returning": "boolean",
-    "last_reported": "int64"
+    "datetime": "2024-01-01",
+    "station_id": "0",
+    "num_bikes_available": 0,
+    "vehicle_type_id_1": 0,
+    "vehicle_type_id_2": 0,
+    "num_ebikes_available": 0,
+    "num_docks_available": 0,
+    "num_docks_disabled": 0,
+    "num_bikes_disabled": 0,
+    "num_scooters_available": 0,
+    "num_scooters_unavailable": 0,
+    "is_installed": True,
+    "is_renting": True,
+    "is_returning": True,
+    "last_reported": 0
 }
 
 schema_station_info = {
-    "station_id": "object",
-    "name": "object",
-    "short_name": "object",
-    "region_id": "object",
-    "lon": "float64",
-    "lat": "float64",
-    "capacity": "int64",
-    "datetime": "datetime64[ns]"
+    "station_id": "0",
+    "name": "0",
+    "short_name": "0",
+    "region_id": "0",
+    "lon": 0.0,
+    "lat": 0.0,
+    "capacity": 0,
+    "datetime": "0"
 }
 
 schema_weather = {
-    "datetime": "datetime64[ns]",
-    "temperature_2m": "float64",
-    "wind_speed_10m": "float64",
-    "relative_humidity_2m": "float64",
-    "precipitation": "float64",
-    "dew_point_2m": "float64",
-    "rain": "float64",
-    "showers": "float64",
-    "snowfall": "float64",
-    "wind_gusts_10m": "float64",
-    "sunshine_duration": "float64",
-    "visibility": "float64",
-    "diffuse_radiation": "float64",
-    "station_id": "object"
+    "datetime": "2024-01-01",
+    "temperature_2m": 0.0,
+    "wind_speed_10m": 0.0,
+    "relative_humidity_2m": 0.0,
+    "precipitation": 0.0,
+    "dew_point_2m": 0.0,
+    "rain": 0.0,
+    "showers": 0.0,
+    "snowfall": 0.0,
+    "wind_gusts_10m": 0.0,
+    "sunshine_duration": 0.0,
+    "visibility": 0.0,
+    "diffuse_radiation": 0.0,
+    "station_id": "0"
 }
 
 # Create empty DataFrames
-df_station_status_nrt = pd.DataFrame({k: pd.Series(dtype=v) for k, v in schema_station_status_nrt.items()})
-df_station_info = pd.DataFrame({k: pd.Series(dtype=v) for k, v in schema_station_info.items()})
-df_weather = pd.DataFrame({k: pd.Series(dtype=v) for k, v in schema_weather.items()})
+df_station_status_nrt = pd.DataFrame(schema_station_status_nrt) #pd.DataFrame({k: pd.Series(dtype=v) for k, v in schema_station_status_nrt.items()})
+df_station_info = pd.DataFrame(schema_station_info) #pd.DataFrame({k: pd.Series(dtype=v) for k, v in schema_station_info.items()})
+df_weather = pd.DataFrame(schema_weather) #pd.DataFrame({k: pd.Series(dtype=v) for k, v in schema_weather.items()})
 
 # Define the path of the GCS folders for the Delta tables
 ssn_path = "gs://delta_table_bucket/station_status_nrt_delta_table"
